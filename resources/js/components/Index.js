b9d4b9e16287ccee91d4d857986bf06300348c11
Václav Gazda <gazdavaclav@gmail.com>
2021-09-08 17:48:58 +0200

    lmao xdd german

--- resources/js/components/Index.js
+++ resources/js/components/Index.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 
-function Index() {
+
+const  Index = () => {
+    const [kokot, setKokot] = useState('');
+    useEffect( async () => {
+        const res = await fetch('http://127.0.0.1:8000/api/test');
+        const data = await res.json();
+
+        console.log(data);
+    }, []);
+
+    
     return (
         <div className="container">
             <div className="row justify-content-center">
                 <div className="col-md-8">
                     <div className="card">
-                        <div className="card-header">Example Component</div>
-
+                        <div className="card-header">Example asd asdasdsddadsd  fsdhgdfhnhasdahoj</div>
+                            
                         <div className="card-body">I'm an example component!</div>
                     </div>
                 </div>
