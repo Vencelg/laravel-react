f887dbdabc36b4d1ec5d213df9531a76ed879e1a
Vencelg <gazdavaclav@gmail.com>
2022-01-15 19:24:54 +0100

    kokot komit

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,6 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
 {{ $slot }}
 @endif
