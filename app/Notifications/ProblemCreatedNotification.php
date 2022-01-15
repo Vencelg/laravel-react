dc13fdf47e17d43ce409346cf325e25a9d591db2
Vencelg <gazdavaclav@gmail.com>
2022-01-15 19:40:57 +0100

    making mail faster 2

--- app/Notifications/ProblemCreatedNotification.php
+++ app/Notifications/ProblemCreatedNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class ProblemCreatedNotification extends Notification
+class ProblemCreatedNotification extends Notification implements shouldQueue
 {
     use Queueable;
 
