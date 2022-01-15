7a5c83c72b29eb0db9b4e4dc52f1106ff1e58422
Vencelg <gazdavaclav@gmail.com>
2022-01-15 21:54:21 +0100

    Styling emails

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
             @if (trim($slot) === 'Creative Hill College')
-                <img src="{{asset('images/chcLogo.png')}}" class="logo" alt="CHC Logo">
+                <img src="{{asset('public/images/chcLogo.png')}}" class="logo" alt="CHC Logo">
             @else
                 {{ $slot }}
             @endif
